01662c29ae1026cfc6d6069504f2a5e3e7d9ed46
Suzy <suzy.nakayama@gmail.com>
2020-02-14 15:40:43 -0500

    Updates

---
+++ precache-manifest.4ecdcf4ece6dcd7996362be0264f66a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9117312960b042112988760574af16c",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "ac1f4eb5908aec6cbc7b",
+    "url": "/project-4-assessment/static/css/main.7676c424.chunk.css"
+  },
+  {
+    "revision": "50791072ca96be2954d2",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1f4eb5908aec6cbc7b",
+    "url": "/project-4-assessment/static/js/main.89a3149f.chunk.js"
+  },
+  {
+    "revision": "9923933f7998bf1ba10e",
+    "url": "/project-4-assessment/static/js/runtime-main.6f46026f.js"
+  }
+]);
