367eddbeeedf39a766e4673735d5776be7c6e048
Suzy <suzy.nakayama@gmail.com>
2020-02-14 15:43:14 -0500

    Updates

---
+++ precache-manifest.0290fb5955dfd4aaa45538ecaa455849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c481ef637ccafc031568933dc9c4339",
+    "url": "/assessment4/index.html"
+  },
+  {
+    "revision": "be766854b0d36f55e71a",
+    "url": "/assessment4/static/css/main.7676c424.chunk.css"
+  },
+  {
+    "revision": "50791072ca96be2954d2",
+    "url": "/assessment4/static/js/2.4a494911.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/assessment4/static/js/2.4a494911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be766854b0d36f55e71a",
+    "url": "/assessment4/static/js/main.836d9c48.chunk.js"
+  },
+  {
+    "revision": "92cd0a68023134acb07d",
+    "url": "/assessment4/static/js/runtime-main.a8ca0b94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-4-assessment/precache-manifest.4ecdcf4ece6dcd7996362be0264f66a4.js"
+  "/assessment4/precache-manifest.0290fb5955dfd4aaa45538ecaa455849.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-4-assessment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/assessment4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
